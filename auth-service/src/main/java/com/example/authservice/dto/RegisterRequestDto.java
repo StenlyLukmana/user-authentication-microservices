4f548a23da61e30c96ae432e4ce628db30bfb744
unknown <stenlylukmanaa@gmail.com>
2025-11-18 13:33:15 +0700

    Imported dependency

--- auth-service/src/main/java/com/example/authservice/dto/RegisterRequestDto.java
+++ auth-service/src/main/java/com/example/authservice/dto/RegisterRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.authservice.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
